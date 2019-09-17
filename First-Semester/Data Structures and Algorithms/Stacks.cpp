6476f1eb2e65d4bd69e534e0fd398b0498489524
VMangahis <vince.baron092299@gmail.com>
2019-09-18 00:04:31 +0800

    updated

--- First-Semester/Data Structures and Algorithms/Stacks.cpp
+++ First-Semester/Data Structures and Algorithms/Stacks.cpp
@@ -2,78 +2,64 @@
 
 #include<iostream>
 #include<string>
+#include<algorithm>
 using namespace std;
 
 struct Stack{
-	
-	char cm[150];
+
+	char *cm;
 	int size;
+	bool correctSyntax = false;
 	void push(string command)
 	{
 			size = command.length();
-				char getString[] = {'g','e','t','l','i','n','e', '(','c','i','n',',' };
+			cm = new char[size];
 			for(int x = 0; x < command.length() ; x++)
 			{
-				if(command[x] == ' ')
-				{
-					cm[x] = getString[x];
-				}
 				cm[x] = command[x];
 			}
-		
-	}	
-	
-	
+
+	}
+
+
 	void pop(){
 		int counter = 0;
 		char getString[] = {'g','e','t','l','i','n','e', '(','c','i','n',',' };
-	
-		for(int a = 0; a < size; a++)
-		{
-			if(cm[a] != getString[counter] && counter < 12)
-			{
-				if(cm[a] == ' ')
-				{
-					cm[a] = getString[counter];
-					counter=0;
-					a=-1;
-					if(counter < 0)
-					{
-						counter = 0;
-					}
-					continue;
-					
-				}
-				
-				
-				
-				
-				else{
-					cout << "Syntax Error" << endl;
-					break;
-					}
-			}
-			
-			
-			
 
-			
-			
-			
-			
-			if(a== size-1 && cm[a] == ';'){
-				cout << "Correct Syntax" << endl;
-				break;
-			}
-			
-			counter++;
-		}
-			
-			
-			
-			
-		}
-	
+        if(cm[size-1] != ';' || cm[size-2] != ')')
+        {
+            correctSyntax = false;
+            return;
+        }
+
+
+		for(int a = 0; a < 12; a++)
+		{
+                if(cm[a] != getString[a])
+                {
+
+                    correctSyntax = false;
+                    return;
+
+                }
+        }
+
+        correctSyntax = true;
+
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
 };
 
 
@@ -84,9 +70,17 @@ int main()
 	string com;
 	cout << "Input getline commmand" << endl;
 	getline( cin, com  );
+	com.erase(remove(com.begin(),com.end(), ' '), com.end());
 	command.push(com);
 	command.pop();
-		
+	if(command.correctSyntax)
+    {
+        cout << "Valid Syntax" << endl;
+    }
+    else{
+        cout << "Syntax Error " << endl;
+    }
+
 }
 
 
