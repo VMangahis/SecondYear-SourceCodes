5384b15d0c0ee92de60d9a08d9c400ee67761de0
VMangahis <vince.baron092299@gmail.com>
2020-01-05 13:40:46 +0800

    updated

--- Second-Semester/OOP/Activitiy1/src/LargeString.java
+++ Second-Semester/OOP/Activitiy1/src/LargeString.java
@@ -12,13 +12,13 @@ public class LargeString {
 			originalString = input.nextLine();
 			System.out.println("Input how many copies of this string.");
 			number = input.nextInt();
-			while(number < 0)
+			while(number < 0)					// Validation in case input ni user ay less than 0
 			{
 				System.out.println("Number of copies cannot be a negative number. Please try again.");
 				number = input.nextInt();
 			}
 			for(int a = 0; a < number ; a++)
-				largerString+=originalString; // Concatenation
+				largerString+=originalString; // Concatenation(pagkakabit ng string sa isa't isa)
 			
 			System.out.printf("Original String: %s \nLarger String: %s", originalString, largerString);
 			System.exit(0);
--- Second-Semester/OOP/Activitiy1/src/Temperature.java
+++ Second-Semester/OOP/Activitiy1/src/Temperature.java
@@ -6,18 +6,18 @@ public class Temperature {
 		String f;
 		Float fNumber;
 		Float celsius;
-		f = JOptionPane.showInputDialog(null, "Input temperature in Fahrenheit.", 0.0);
-		if(f == null)
+		f = JOptionPane.showInputDialog(null, "Input temperature in Fahrenheit.", 0.0);  // String lagi ang nagiging dating ng iniinput natin sa GUI
+		if(f == null)											// In case walang iinput si user exit na
 		{
 			System.exit(0);
 		}
-		fNumber = Float.parseFloat(f);
-		while(f == null || fNumber == null)
+		fNumber = Float.parseFloat(f);							// Yung string na number na ininput ni user ay coconvert natin sa float
+		while(f == null || fNumber == null)						//Validation lang in case letter iinput ni user
 		{
-			JOptionPane.showInputDialog(null, "Error Input!\nInput temperature in Fahrenheit.");
+			JOptionPane.showInputDialog(null, "Error Input!\nInput temperature in Fahrenheit.");    
 		}
-		celsius = (5*(fNumber-32))/9;
-		JOptionPane.showMessageDialog(null, "Fahrenheit :" + fNumber + "\nCelsius :" + celsius);
+		celsius = (5*(fNumber-32))/9;							//Conversion
+		JOptionPane.showMessageDialog(null, "Fahrenheit :" + fNumber + "\nCelsius :" + celsius); // Output
 
 	}
 
