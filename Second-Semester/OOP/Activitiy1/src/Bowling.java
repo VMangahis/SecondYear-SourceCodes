ea235f9d3dc6a3b8828f91f78f366fe4c08b35f1
Vince Mangahis <vince.baron092299@gmail.com>
2020-01-07 22:50:26 +0800

    updated

--- Second-Semester/OOP/Activitiy1/src/Bowling.java
+++ Second-Semester/OOP/Activitiy1/src/Bowling.java
@@ -3,11 +3,10 @@ import java.util.Scanner;
 public class Bowling {
 
 	public static void main(String[] args) {
-		// Perfect = 300 Score
 		Scanner input = new Scanner(System.in);
 		int[][] rolls = new int[10][2];
 		int frameScore = 0;
-		int pins = 10;
+		int defaultPins = 10;
 		int totalScore = 0;
 		boolean isStrike = false;
 		boolean isSpare = false;
@@ -16,27 +15,27 @@ public class Bowling {
 		for(int frame = 0 ; frame < 10 ; frame++)
 		{
 			System.out.println("Frame: " + (frame+1));
-			pins = 10;
+			
 			for(int roll = 0; roll < 2; roll++)
 			{
 				System.out.println("Roll: " + (roll+1));
-				System.out.println("Number of pins knocked down (" + (pins) + " left):" );
+				System.out.printf("Number of pins knocked down (%d left)", defaultPins);
 				System.out.println("Score: " + totalScore);
 				rolls[frame][roll] = input.nextInt();
-				while(rolls[frame][roll] > pins || rolls[frame][roll] < 0)
+				while(rolls[frame][roll] > defaultPins || rolls[frame][roll] < 0)
 				{
-					System.out.println("Input cannot be greater than the amount of pins left or a negative number. Please try again.");
-					System.out.println("Number of pins knocked down (" + (pins) + " left):" );
+					System.out.println("Greater than pins left or negative number is not allowed. Please try again");
+					System.out.printf("Number of pins knocked down (%d left)", defaultPins);
 					rolls[frame][roll] = input.nextInt();
 				}
 				
 				if(roll == 0 && isSpare)
 				{
-					totalScore+= (rolls[frame][roll]+ 10); 	// 10 + next number of pins knock down 
+					totalScore+= (rolls[frame][roll]+ 10); 	 
 					isSpare = false;
 				}
 				
-				if(roll == 1 && isStrike)		// 10 + total pins knocked down in the next frame 
+				if(roll == 1 && isStrike)		 
 				{
 					totalScore += (rolls[frame][roll]+ rolls[frame][roll-1] + 10);  
 					isStrike = false;
@@ -44,15 +43,15 @@ public class Bowling {
 				
 				
 				
-				pins-=rolls[frame][roll];
+				defaultPins-=rolls[frame][roll];
 				
-				if(pins == 0 && roll == 0) 		// Strike
+				if(defaultPins == 0 && roll == 0) 		// Strike
 				{
 					System.out.println("STRIKE!");
 					isStrike = true;
 					break;
 				}
-				else if(pins == 0 && roll == 1) // Spare
+				else if(defaultPins == 0 && roll == 1) // Spare
 				{
 					System.out.println("SPARE!");
 					isSpare = true;
@@ -63,7 +62,10 @@ public class Bowling {
 					totalScore+=(rolls[frame][roll] + rolls[frame][roll-1]);
 				}
 			}
+			defaultPins = 10;
 		}
+		System.out.println("Game end!");
+		System.out.printf("Your final score is: %d", totalScore);
 	}
 
 }
